2929c263eceb8fb91ee7d943c5601b98fd3afe11
imansafari1991 <imansafari33@gmail.com>
2023-11-30 12:54:49 +0330

    remove text block

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,19 +23,6 @@ namespace WPFTutorial
 
 		private void btnFire_Click(object sender, RoutedEventArgs e)
 		{
-			//MessageBox.Show("Could Not Open File ...", "ERROR!", MessageBoxButton.OK, MessageBoxImage.Error);
-			MessageBoxResult result = MessageBox.Show("Do you agree ?", "Agreement", MessageBoxButton.YesNo, MessageBoxImage.Question);
-
-			if (result == MessageBoxResult.Yes)
-			{
-				tbInfo.Text = "Agreed";
-			}
-
-			else
-			{
-				tbInfo.Text = "NotAgreed";
-			}
-
 
 		}
 	}
